67abb622d3076d36b9892bd7d073da6efb469160
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-05 17:58:10 +0800

    加入canvas

--- src/com/mygame/view/Display.java
+++ src/com/mygame/view/Display.java
@@ -1,12 +1,14 @@
 package com.mygame.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Administrator on 2018/1/4.
  */
 public class Display {
 	private JFrame jFrame;
+	private Canvas canvas;
 	private String title;
 	private int width;
 	private int height;
@@ -26,5 +28,13 @@ public class Display {
 		jFrame.setResizable(false);
 		jFrame.setLocationRelativeTo(null);
 		jFrame.setVisible(true);
+
+		canvas = new Canvas();
+		canvas.setPreferredSize(new Dimension(width, height));
+		canvas.setMaximumSize(new Dimension(width, height));
+		canvas.setMinimumSize(new Dimension(width, height));
+
+		jFrame.add(canvas);
+		jFrame.pack();
 	}
 }
