740f539d7b5773f12930538b581292ccd33019e3
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-05 18:27:49 +0800

    引入线程Runnable接口

--- src/com/mygame/make/Game.java
+++ src/com/mygame/make/Game.java
@@ -5,15 +5,64 @@ import com.mygame.view.Display;
 /**
  * Created by Administrator on 2018/1/5.
  */
-public class Game {
-	private Display display;
+public class Game implements Runnable {
 
+	private Display display;
 	public int width, height;
+	public String title;
+
+	private boolean running;
+	private Thread thread;
 
 	public Game(String title, int width, int height){
 		this.width = width;
 		this.height = height;
+		this.title = title;
+		running = false;
+	}
 
+	private void init(){
 		display = new Display(title, width, height);
 	}
+
+	private void tick(){
+
+	}
+
+	private void render(){
+
+	}
+
+	public void run(){
+
+		init();
+
+		while(running){
+			tick();
+			render();
+		}
+
+		stop();
+
+	}
+
+	public synchronized void start(){
+		if(running)
+			return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+
+	public synchronized void stop(){
+		if(!running)
+			return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
