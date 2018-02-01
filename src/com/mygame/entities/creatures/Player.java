278b4eaf15116405b4a4216d173b4df8a07f2b99
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-01 14:19:30 +0800

    角色动画测试

--- src/com/mygame/entities/creatures/Player.java
+++ src/com/mygame/entities/creatures/Player.java
@@ -1,25 +1,42 @@
 package com.mygame.entities.creatures;
 
+import com.mygame.gfx.Animation;
 import com.mygame.gfx.Assets;
 import com.mygame.make.Handler;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by Administrator on 2018/1/30.
  */
 public class Player extends Creature {
+	//Animations
+	private Animation animDown, animUp, animLeft, animRight;
+
 	public Player(Handler handler, float x, float y) {
 		super(handler, x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
 
-		bounds.x = 16;
-		bounds.y = 32;
-		bounds.width = 32;
-		bounds.height = 32;
+		bounds.x = 22;
+		bounds.y = 44;
+		bounds.width = 19;
+		bounds.height = 19;
+
+		//Animatons
+		animDown = new Animation(500, Assets.player_down);
+		animUp = new Animation(500, Assets.player_up);
+		animLeft = new Animation(500, Assets.player_left);
+		animRight = new Animation(500, Assets.player_right);
 	}
 
 	@Override
 	public void tick() {
+		//Animations
+		animDown.tick();
+		animUp.tick();
+		animRight.tick();
+		animLeft.tick();
+		//Movement
 		getInput();
 		move();
 		handler.getGameCamera().centerOnEntity(this);
@@ -41,6 +58,18 @@ public class Player extends Creature {
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.player, (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+		g.drawImage(getCurrentAnimationFrame(), (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+	}
+
+	private BufferedImage getCurrentAnimationFrame() {
+		if (xMove < 0) {
+			return animLeft.getCurrentFrame();
+		} else if (xMove > 0) {
+			return animRight.getCurrentFrame();
+		} else if (yMove < 0) {
+			return animUp.getCurrentFrame();
+		} else {
+			return animDown.getCurrentFrame();
+		}
 	}
 }
